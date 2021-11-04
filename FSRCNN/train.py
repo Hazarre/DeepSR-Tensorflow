8ac6f770d3245cde4bbf39466a83c0d5e6845990
Hazarre <henry110688@gmail.com>
2021-11-04 14:20:20 +0800

    training procedure update

--- FSRCNN/train.py
+++ FSRCNN/train.py
@@ -1,37 +1,86 @@
-from model import FSRCNN
+import os 
+import sys
 
 from tensorflow import keras
 import tensorflow as tf
-from FSRCNN.dataset import deserialize
+from PIL import Image
+import numpy as np
+from model import FSRCNN
 
+# Allow local import from parent directory
+sys.path.insert(0, "..")
+from dataset import deserialize
+from common import normalize_y
+
+SIZE = 100
+CHN  = 1
+
+def preprocess(example): 
+    lr, hr = deserialize(example)
+    hr = normalize_y(hr)    
+    lr = normalize_y(lr)
+    return lr, hr
+
+
+# config and parameters 
+IS_FSRCNN_S = True    # is FSRCNN_S or FSRCNN
+RESUME = True    # Train from scratch or use previously traind weights 
 n_tfrecords = 32
-lr 
-
-# prep the model 
-model = FSRCNN()
-loss_fn = tf.keras.losses.MeanSquaredError()
-optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+batch_size = 16
+epochs = 50
 
 # prep the dataset
-train_dir = [f"tfrecords/train{i}.tfrecords" for i in range(n_tfrecords)]
-valid_dir = [f"tfrecords/train{i}.tfrecords" for i in range(n_tfrecords)]
+train_dir = [f"../tfrecords/div2k_train{i}.tfrecords" for i in range(n_tfrecords)]
+valid_dir = [f"../tfrecords/div2k_valid{i}.tfrecords" for i in range(n_tfrecords)]
+train_dataset = tf.data.TFRecordDataset(train_dir).map(preprocess).batch(batch_size)
+valid_dataset = tf.data.TFRecordDataset(valid_dir).map(preprocess).batch(batch_size)
 
-train_ds = tf.data.TFRecordDataset(train_dir).map(deserialize)
-valid_ds = tf.data.TFRecordDataset(valid_dir).map(deserialize)
+# prep the model 
+model = FSRCNN(d=32, s=5, m=1, r=4) if IS_FSRCNN_S else FSRCNN()
+loss_fn = tf.keras.losses.MeanSquaredError()
+optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+model.compile(optimizer, loss=loss_fn)
 
-# epochs = 50
+name_model = "FSRCNN_S" if IS_FSRCNN_S else "FSRCNN"
+log_dir = f"logs/{name_model}"       
+checkpoint_path  = f"checkpoints/"+ name_model+"{epoch:03d}.ckpt"
 
+
+if RESUME: 
+    # load pre-trained model 
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    model.load_weights(latest)
+
+# Training
 callbacks = [
-    keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+    tf.keras.callbacks.EarlyStopping(monitor = "val_loss", 
+                                     patience = 3, 
+                                     restore_best_weights = True),
+
+    tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                        verbose = 1,
+                                        monitor="loss",
+                                        save_freq="epoch"),
+
+    tf.keras.callbacks.TensorBoard(log_dir=log_dir, update_freq="epoch")
 ]
 
-model.compile(optimizer, loss=loss_fn)
-model.fit( train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds)
+history = model.fit(train_dataset, 
+           initial_epoch=40, 
+           epochs=epochs, 
+           callbacks=callbacks, 
+           validation_data=valid_dataset)
 
-# 91 image dataset for training 
-# general 100 image data set for fine tuning 
 
-# learning rate of the convolution layer is 10^-3 
-# 10#-4 for deconv layers 
-# during fine-tuning the learning rate of all layers is reduced by half. 
+def get_latest_model(): 
+    model = FSRCNN(d=32, s=5, m=1, r=4) if IS_FSRCNN_S else FSRCNN()
+    loss_fn = tf.keras.losses.MeanSquaredError()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+    model.compile(optimizer, loss=loss_fn)
 
+    name_model = "FSRCNN_S" if IS_FSRCNN_S else "FSRCNN"
+    checkpoint_path  = f"checkpoints/"+ name_model+"{epoch:03d}.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    model.load_weights(latest)
--- common.py
+++ common.py
@@ -5,6 +5,8 @@ from skimage.color import rgb2ycbcr, ycbcr2rgb
 # Global Variables 
 SIZE = 100  # height and width of training samples
 CHN = 1     # number of channels of input/output channels 1 for y and 3 for rgb
+R = 4       # The upscaling ratio
+
 
 DIV2K_RGB_MEAN = np.array([0.4488, 0.4371, 0.4040]) * 255
 
